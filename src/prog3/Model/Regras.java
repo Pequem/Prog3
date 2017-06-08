bda3138b854878d61b6ee0fc4566564c3850760c
kaiorosa1 <kaiosrosa@gmail.com>
2017-06-08 00:53:20 -0300

    KaioRosaTest02

---
+++ src/prog3/Model/Regras.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog3.Model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author user
+ */
+public class Regras {
+    private double fatorMult;
+    private Date dataInicio;
+    private Date dataFim;
+    private int qtdAnos;
+    private int pontuacaoMin;
+    private ArrayList<Pontuacao> pontuacao;
+    private ArrayList<Qualis> qualis;//verificar essa associacao later on
+    
+    
+}
