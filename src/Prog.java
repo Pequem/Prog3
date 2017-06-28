9b4eaff113a6ab6b2f7626fb9470e160613f234e
kaiorosa1 <kaiosrosa@gmail.com>
2017-06-28 08:41:00 -0300

    032

--- src/Prog.java
+++ src/Prog.java
@@ -20,38 +20,66 @@ public class Prog {
         //---------------- LISTAS/HashMaps------------------
         //Scanner s = new Scanner(System.in);
         //s.close();
-        /*os parametros podem ser passados de qualquer forma
-                sempre organizalos antes de ler os arquivos
-         */
+        
+        int leu=0;
+        int optionRead=0,optionWrite=0;
+        
+        
         //LEITURA DOS ARQUIVOS
-        for (int j = 2; j < args.length;j++) {
-            switch (args[j]) {
-                case "-d":
-                    c.ReadDocentes(args[++j]);
-                    break;
-                case "-v":
-                    c.ReadVeiculos(args[++j]);
-                    break;
-                case "-p":
-                    c.ReadPublicacoes(args[++j]);
-                    break;
-                case "-q":
-                    c.ReadQualis(args[++j]);
-                    break;
-                case "-r":
-                    c.ReadRegras(args[++j]);
-                    break;
-                case "-a":
-                    c.ReadAnoCredenciamento(args[++j]);
-                    break;
-                default:
-                    System.out.println("Comando " + args[j] + " é invalido");
-                    return;
+        for(int k = 2; k< args.length;k++){
+            if(args[k].compareTo("--read-only") == 0){
+                optionRead=1;
+            }
+            if(args[k].compareTo("--write-only") == 0){
+                optionWrite=1;
             }
         }
+        if((optionRead ==0) || (optionRead==1)){
+            for (int k = 2;k < args.length;k++){
+                for (int j = 2; j < args.length;j++) {
+                    if((leu == 0) && (args[j].compareTo("-d") == 0)){
+                            c.ReadDocentes(args[++j]);
+                            leu++;
+                    }
+                    else if((leu == 1) && (args[j].compareTo("-v") == 0)){
+                            c.ReadVeiculos(args[++j]);
+                            leu++;
+                    }
+                    else if((leu == 2) && (args[j].compareTo("-p") == 0)){
+                            c.ReadPublicacoes(args[++j]);
+                            leu++;
+                    }
+                    else if((leu == 3) && (args[j].compareTo("-q") == 0)){
+                            c.ReadQualis(args[++j]);
+                            leu++;
+                    }
+                    else if((leu == 4) && (args[j].compareTo("-r") == 0)){
+                            c.ReadRegras(args[++j]);
+                            leu++;
+                    }
+                    else if((leu == 5) && (args[j].compareTo("-a") == 0)){
+                            c.ReadAnoCredenciamento(args[++j]);
+                            leu++;
+                    }
+
+                }
+            }
+            
+            if(optionRead == 1){
+                //READ ONLY OPTION
+                
+                
+            }
         
-       c.WriteRecredenciamentoFile();
-       c.WriteListaPublicacoes();
-       c.WriteStatistics();
+        }
+       
+        if(optionWrite ==0){
+            c.WriteRecredenciamentoFile();
+            c.WriteListaPublicacoes();
+            c.WriteStatistics();
+        }
+        if(optionWrite ==1){
+            //WRITE ONLY OPTION
+        }
     }
 }
